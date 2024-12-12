4e0c0096f60b8f121cdbe7c7e62cff4ffcf1ff0d
koksing456 <koksing456@gmail.com>
2024-12-12 22:58:34 +0800

    fail tailwind

--- stanford-prison/tailwind.config.js
+++ stanford-prison/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+
 export default {
   content: [
     "./index.html",
@@ -8,7 +11,7 @@ export default {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
+    forms,
+    typography,
   ],
 } 
